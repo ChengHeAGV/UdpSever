7fd4275f2e28a7168fc8ba59e400aa62dc6f3916
公司电脑 <sunyiming537@126.com>
2018-03-28 17:53:18 +0800

    任务调度-任务列表设计完成

--- DispatchSystem/User/TaskForm.cs
+++ DispatchSystem/User/TaskForm.cs
@@ -1,4 +1,5 @@
-﻿using Modbus.Device;
+﻿using AdvancedDataGridView;
+using Modbus.Device;
 using Modbus.IO;
 using System;
 using System.Collections.Generic;
@@ -33,7 +34,7 @@ namespace DispatchSystem.User
         /// <summary>
         /// 任务状态
         /// </summary>
-        enum State
+        enum TaskState
         {
             Waiting, //等待
             Runing,  //运行中
@@ -43,16 +44,26 @@ namespace DispatchSystem.User
         /// <summary>
         /// 任务
         /// </summary>
-        struct Task
+        class Task
         {
-            int SerialNum;//任务编号
-            int State;//任务状态
+            public int TaskNum; //任务编号
+            public int TaskState; //任务状态
+            public DateTime CmdTime;//任务下发时间
+            public int AgvNum; //AGV编号
+            public int Connect; //通信状态
+            public int Route;//当前路径
+            public int Station;//当前站点
+            public int Power;//电量
+            public int Speed;//速度
+            public int AgvState; //Agv状态
+            public int Error;//报警信息
         }
 
         //任务列表
         List<Task> tasks = new List<Task>();
         private void TaskForm_Load(object sender, EventArgs e)
         {
+            #region ModbusTcp 代码调试
             //TcpClient tcpClient = new TcpClient("192.168.10.106", 502);
             //ModbusIpMaster master = ModbusIpMaster.CreateIp(tcpClient);
 
@@ -80,7 +91,57 @@ namespace DispatchSystem.User
             //{
             //    UdpSever.Shell.WriteError("debug", ex.ToString());
             //}
+            #endregion
 
+            #region 任务列表测试
+            //增加列
+            dataGridView1.Columns.Add("1", "任务编号");
+            var index = dataGridView1.Columns.Add("2", "MES下发时间");
+            dataGridView1.Columns[index].Width = 130;
+            dataGridView1.Columns.Add("3", "任务状态");
+            dataGridView1.Columns.Add("4", "AGV编号");
+            dataGridView1.Columns.Add("5", "通信状态");
+            dataGridView1.Columns.Add("6", "当前路径");
+            dataGridView1.Columns.Add("7", "当前站点");
+            dataGridView1.Columns.Add("8", "电量%");
+            dataGridView1.Columns.Add("9", "速度");
+            dataGridView1.Columns.Add("10", "运行状态");
+            dataGridView1.Columns.Add("11", "报警信息");
+
+            //增加行
+            for (int i = 0; i < 10; i++)
+            {
+                Task task = new Task();
+                task.TaskNum = i;
+                task.CmdTime = DateTime.Now;
+                task.TaskState = (int)TaskState.Runing;
+                task.AgvNum = 1;
+                task.Connect = 2;
+                task.Route = 2;
+                task.Station = 2;
+                task.Power = 2;
+                task.Speed = 2;
+                task.AgvState = 2;
+                task.Error = 2;
+                tasks.Add(task);
+            }
+
+            foreach (var item in tasks)
+            {
+                index = this.dataGridView1.Rows.Add();
+                this.dataGridView1.Rows[index].Cells[0].Value = item.TaskNum;
+                this.dataGridView1.Rows[index].Cells[1].Value = item.CmdTime.ToString("yyyy-MM-dd HH:mm:ss");
+                this.dataGridView1.Rows[index].Cells[2].Value = item.TaskState;
+                this.dataGridView1.Rows[index].Cells[3].Value = item.AgvNum;
+                this.dataGridView1.Rows[index].Cells[4].Value = item.Connect;
+                this.dataGridView1.Rows[index].Cells[5].Value = item.Route;
+                this.dataGridView1.Rows[index].Cells[6].Value = item.Station;
+                this.dataGridView1.Rows[index].Cells[7].Value = item.Power;
+                this.dataGridView1.Rows[index].Cells[8].Value = item.Speed;
+                this.dataGridView1.Rows[index].Cells[9].Value = item.AgvState;
+                this.dataGridView1.Rows[index].Cells[10].Value = item.Error;
+            }
+            #endregion
         }
     }
 }
