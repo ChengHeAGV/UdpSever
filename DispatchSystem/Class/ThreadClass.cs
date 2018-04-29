1dd1dda447175afeb9cb98b84826e8937bfb6940
超级本 <sunyiming537@126.com>
2018-04-29 11:10:06 +0800

    增加Thread扩展类，使用方便，退出实时

---
+++ DispatchSystem/Class/ThreadClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DispatchSystem.Class
+{
+    class ThreadClass
+    {
+        public AutoResetEvent exitEvent;
+        private Thread thread;
+        //定义一个委托
+        public delegate void delegateFun();
+
+        public ThreadClass(delegateFun fun)
+        {
+            exitEvent = new AutoResetEvent(false);
+            thread = new Thread(new ThreadStart(fun));
+        }
+        public void Run()
+        {
+            thread.Start();
+        }
+        public void Stop()
+        {
+            exitEvent.Set();
+            thread.Join();
+        }
+    }
+}
+
+///////////////
+//函数体使用方法
+//ThreadClass udpThread = new ThreadClass(UdpFunc);
+//udpThread.Run();
+//udpThread.Stop();
+//private void Process()
+//{
+//    while (true)
+//    {
+//        Console.WriteLine("do some thing");
+//        if (exitEvent.WaitOne(100))//延时100ms
+//        {
+//            break;
+//        }
+//    }
+//}
