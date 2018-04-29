1b86e99e84017911c4311e63adb179f07e1fa9da
超级本 <sunyiming537@126.com>
2018-04-29 11:13:34 +0800

    取消ini格式存储功能

--- DispatchSystem/Developer/DebugForm.cs
+++ DispatchSystem/Developer/DebugForm.cs
@@ -18,21 +18,21 @@ namespace DispatchSystem.Developer
             dataGridView1.Rows.Clear();
 
             //加载debug配置
-            DataGridViewRow row;
-            DataGridViewTextBoxCell textBoxCell;
-            DataGridViewCheckBoxCell checkBoxCell;
+            //DataGridViewRow row;
+            //DataGridViewTextBoxCell textBoxCell;
+            //DataGridViewCheckBoxCell checkBoxCell;
 
-            foreach (var item in XmlHelper.DebugList)
-            {
-                row = new DataGridViewRow();
-                textBoxCell = new DataGridViewTextBoxCell();
-                textBoxCell.Value = item.Name;
-                checkBoxCell = new DataGridViewCheckBoxCell();
-                checkBoxCell.Value = item.Value;
-                row.Cells.Add(textBoxCell);
-                row.Cells.Add(checkBoxCell);
-                dataGridView1.Rows.Add(row);
-            }
+            //foreach (var item in XmlHelper.DebugList)
+            //{
+            //    row = new DataGridViewRow();
+            //    textBoxCell = new DataGridViewTextBoxCell();
+            //    textBoxCell.Value = item.Name;
+            //    checkBoxCell = new DataGridViewCheckBoxCell();
+            //    checkBoxCell.Value = item.Value;
+            //    row.Cells.Add(textBoxCell);
+            //    row.Cells.Add(checkBoxCell);
+            //    dataGridView1.Rows.Add(row);
+            //}
         }
 
         private void DebugForm_Load(object sender, EventArgs e)
@@ -43,15 +43,15 @@ namespace DispatchSystem.Developer
         private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
             DataGridView dd = sender as DataGridView;
-            if (dd.Rows.Count != 0)
-            {
-                for (int i = 0; i < XmlHelper.DebugList.Count; i++)
-                {
-                    XmlHelper.DebugList[i].Value = (bool)dd.Rows[i].Cells[1].Value;
-                }
-                //更新到xml
-                XmlHelper.UpdataDebug();
-            }
+            //if (dd.Rows.Count != 0)
+            //{
+            //    for (int i = 0; i < XmlHelper.DebugList.Count; i++)
+            //    {
+            //        XmlHelper.DebugList[i].Value = (bool)dd.Rows[i].Cells[1].Value;
+            //    }
+            //    更新到xml
+            //    XmlHelper.UpdataDebug();
+            //}
 
         }
 
@@ -71,21 +71,20 @@ namespace DispatchSystem.Developer
 
         private void buttonCheckAll_Click(object sender, EventArgs e)
         {
-
-            for (int i = 0; i < XmlHelper.DebugList.Count; i++)
-            {
-                XmlHelper.DebugList[i].Value = true;
-            }
-            updateToForm();
+            //for (int i = 0; i < XmlHelper.DebugList.Count; i++)
+            //{
+            //    XmlHelper.DebugList[i].Value = true;
+            //}
+            //updateToForm();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < XmlHelper.DebugList.Count; i++)
-            {
-                XmlHelper.DebugList[i].Value = false;
-            }
-            updateToForm();
+            //for (int i = 0; i < XmlHelper.DebugList.Count; i++)
+            //{
+            //    XmlHelper.DebugList[i].Value = false;
+            //}
+            //updateToForm();
         }
     }
 }
