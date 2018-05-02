e599621078f33f21a617ee22dbfa46d6cf55a494
超级本 <sunyiming537@126.com>
2018-05-02 09:23:04 +0800

    打印控件

---
+++ DispatchSystem/UserControls/ExConsole.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DispatchSystem.UserControls
+{
+    partial class ExConsole
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exListView1 = new DispatchSystem.ExListView();
+            this.SuspendLayout();
+            // 
+            // exListView1
+            // 
+            this.exListView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.exListView1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.exListView1.FullRowSelect = true;
+            this.exListView1.HideSelection = false;
+            this.exListView1.Location = new System.Drawing.Point(0, 0);
+            this.exListView1.Name = "exListView1";
+            this.exListView1.Size = new System.Drawing.Size(150, 150);
+            this.exListView1.TabIndex = 0;
+            this.exListView1.UseCompatibleStateImageBehavior = false;
+            this.exListView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ExConsole
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.exListView1);
+            this.Name = "ExConsole";
+            this.Load += new System.EventHandler(this.ExConsole_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ExListView exListView1;
+    }
+}
---
+++ DispatchSystem/UserControls/ExConsole.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DispatchSystem.UserControls
+{
+    public partial class ExConsole : UserControl
+    {
+        string[] datekey = new string[10];
+        public ExConsole()
+        {
+            InitializeComponent();
+        }
+
+        private void ExConsole_Load(object sender, EventArgs e)
+        {
+            #region 数据列表
+            datekey[0] = "日期";
+            datekey[1] = "时间";
+            datekey[2] = "事件";
+
+            exListView1.FullRowSelect = true;//要选择就是一行
+            exListView1.Columns.Add(datekey[0], 120, HorizontalAlignment.Left);
+            exListView1.Columns.Add(datekey[1], 140, HorizontalAlignment.Left);
+            exListView1.Columns.Add(datekey[2], 680, HorizontalAlignment.Left);
+            #endregion
+        }
+
+        public void WriteLine(string msg,Color color, int fontSize = 14) 
+        {
+            Write(msg, color, fontSize);
+        }
+        public void WriteLine(string msg, int fontSize = 14)
+        {
+            Write(msg, Color.Black, fontSize);
+        }
+
+        public void WriteLine(string msg) 
+        {
+            Write(msg, Color.Black, 14);
+        }
+        public void WriteLine(string format, params object[] args)  
+        {
+            Write(string.Format(format, args), Color.Black, 14);
+        }
+        public void Write(string msg, Color color, int fontSize)
+        {
+            try
+            {
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    ListViewItem item = new ListViewItem();
+                    item.Text = DateTime.Now.ToString("yyyy-MM-dd");//"日期";
+                    item.SubItems.Add(DateTime.Now.ToString("HH:mm:ss fff"));//  "时间";
+                    item.SubItems.Add(msg);//  "事件";
+
+                    item.ForeColor = color;//字体颜色
+                    item.Font = new Font("新宋体", fontSize, FontStyle.Regular); //字体颜色
+
+                    if (exListView1.Items.Count % 2 == 0)
+                        item.BackColor = Color.FromArgb(200, 0xf5, 0xf6, 0xeb);
+
+                    exListView1.Items.Add(item);
+                    exListView1.EnsureVisible(exListView1.Items.Count - 1);//滚动到指定的行位置
+                }));
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
